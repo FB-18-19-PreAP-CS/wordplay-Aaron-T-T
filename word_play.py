6b1b79ba019b1226170c60e34c655cc34367275e
TORRES.AARON <2210153@fbisd.flourbluffschools.net>
2018-11-02 13:12:26 -0500

    Has no e works so does avoid

--- word_play.py
+++ word_play.py
@@ -11,6 +11,13 @@ def at_least():
                 count_the += 1
             if len(word) >= 20:
                 print(word)
+                
+def has_no_e(word):
+    if 'e' in word or 'E' in word:
+        return False
+    else:
+        return True
+
 def no_e():
     with open("words.txt") as file:
         count_the = 0
@@ -18,29 +25,26 @@ def no_e():
         for line in file:
             for word in line.strip().split():
                 count_th += 1
-                if 'e' not in word:
+                if has_no_e(word):
                     count_the += 1
         a = ((count_the / count_th) * 100) // 1
         b = int(a)
         print("{:2}%".format(b))
         
-def avoids():
-    a = ['a','e','b','c']
-    
-    word = input('Say a word: ')
-    for i in range(0,len(word)):
-        if a[i] in word:
+def avoids(word,letters):
+    for i in range(0,len(letters)):
+        if letters[i] in word:
             return False
         else:
             return True
 def avoids_count():
     a = input('Put letters that you don\'t want in the words: ')
-    b = list(a)
     with open("words.txt") as file:
         count = 0
         for line in file:
             for word in line.strip().split():
-                if b[] not in word:
+                if avoids(word,a):
                     count += 1
+    print(count)
 if __name__ == "__main__":
     avoids_count()
