c412ab3e36d92056092ee2e7c9fe739f89a86248
Aaron-T-T <deadkilla67.@gmail.com>
2018-11-02 08:07:18 -0500

    Think 3 is done working on 4

--- word_play.py
+++ word_play.py
@@ -25,8 +25,22 @@ def no_e():
         print("{:2}%".format(b))
         
 def avoids():
+    a = ['a','e','b','c']
+    
     word = input('Say a word: ')
-    print(word)
-
+    for i in range(0,len(word)):
+        if a[i] in word:
+            return False
+        else:
+            return True
+def avoids_count():
+    a = input('Put letters that you don\'t want in the words: ')
+    b = list(a)
+    with open("words.txt") as file:
+        count = 0
+        for line in file:
+            for word in line.strip().split():
+                if b[] not in word:
+                    count += 1
 if __name__ == "__main__":
-    avoids()
+    avoids_count()
